471d3afe7955cb87d4ab70536b67225fe2cda512
阿布天 <renbuqi@163.com>
2016-11-22 14:37:11 +0800

    提交修改

--- controller/shake.js
+++ controller/shake.js
@@ -1,10 +1,11 @@
 var wechat = require('wechat-enterprise');
-var api = new wechat.API($.config.enterprise.corpId, $.config.enterprise.corpsecret);
+var api = new wechat.API($.config.enterprise.corpId, $.config.enterprise.corpsecret,'41');
 
 exports.gettugofwar = function(req, res) {
 	var code = req.query.code;
 	var state = req.query.state;
     var data;
+    api.
     api.getUserIdByCode(code, (data)=>{
     	data = data;
     	console.log(data);
