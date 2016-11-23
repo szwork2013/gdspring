45026efed895e2d3830c1e11f8c48c36ca38e2af
阿布天 <renbuqi@163.com>
2016-11-23 18:01:32 +0800

    提交签到页面逻辑

--- config.js
+++ config.js
@@ -2,25 +2,11 @@
 var config = module.exports = {
     cryptsalt : "gold-tech",
     // 数据库配置
-    db: {
-        mysql: {
-            gd: {
-                host: '172.28.189.101',
-                user: 'root',
-                password: 'P@ssword0',
-                database: 'szzx',
-                port: 3306,
-                multipleStatements: true
-            }
-        }
-    },
     redis: {
-        // Config for oauthserver
-        oauthserver:{
+        server:{
             host: "172.28.189.101",
             port: 6379,
-            db: 0,
-            pass: 'P@ssword0'
+            db: 8
         }
     },
     enterprise: {
@@ -38,7 +24,6 @@ var config = module.exports = {
     // 入口服务器地址
     nginxUrl: "http://192.168.51.4",
     //testhostname: "http://172.28.184.75:9091",
-    testhostname: "http://localhost:9091",
     // 程序路由结构
     routes: {
         get: {
@@ -63,30 +48,7 @@ var config = module.exports = {
         ],
         // 缓存过期时间单位秒, 一般为2个小时有效期
         "max-age": 3600 * 48
-    },
-    //oauth store Type: redis , memory 
-    oauth_store_type:"redis", 
-    // websocket配置
-    socket: {
-        // socket端口
-        port: 8989,
-        // socket连接池
-        clients: {},
-        // socket用户池
-        users: [],
-        // 缓存消息
-        message: {},
-        // 自定义监听
-        listeners: {
-        }
-    },
-    backenduser:{
-        id: "cb96ab39-9ce5-11e6-aef2-000c29dd04de",
-        name:"admin",
-        password:"123456"
-    },
-    //uploadpath :'/Users/tianhuaren/Downloads/'
-    uploadpath :'/var/svn/gOAuth.git/temp/'
+    }
 };
 
 
--- controller/signup.js
+++ controller/signup.js
@@ -1,22 +1,81 @@
-var wechat = require('wechat-enterprise');
-var fs =require("fs");
+var wechat = require('wechat-enterprise'),
+    fs =require("fs"),
+    async = $.async,
+    util = require('util'),
+    redis = $.plug.redis.redisserver;
 
 var api = new wechat.API($.config.enterprise.corpId, $.config.enterprise.corpsecret,"41");
+var KEY = {
+    USER         : 'user:%s',
+};
 
 exports.getreg = function(req, res) {
 	var code = req.query.code;
 	var state = req.query.state;
 
  	api.getUserIdByCode(code, (err, data)=>{
-	    api.getUser(data.UserId, function(err,result){
-	    	if(result.errcode!=0){
-	    		result = {};
-	    		$.extend(result,{avatar:"http://shp.qpic.cn/bizmp/ic7zogWndTyXMsdxYl6QLXpamFlm9FbvzPSokoV6F5BWibqBtK8lTU6Q/"});
-	    		console.log(result);
-	    		res.render('page/signup',result);
-	    		return;
-	    	}
-	    	res.render('page/signup',result);
-	    });
+ 		var result = {};
+		if(data.errcode == 0){
+		   var keyid = util.format(KEY.USER, result.userid);
+		   redis.lpush("reg", keyid, (err, data) => {
+		   });
+
+		   redis.hgetall(keyid, (err, data){
+		   	  res.render('page/signup',data);
+		   });
+		}else
+		{
+		   var keyid = util.format(KEY.USER, "renth");
+		   redis.lpush("reg", keyid, (err, data) => {
+		   });
+		   $.extend(result,{avatar:"http://shp.qpic.cn/bizmp/ic7zogWndTyXMsdxYl6QLXpamFlm9FbvzPSokoV6F5BWibqBtK8lTU6Q/"});
+		   res.render('page/signup',result);
+		}
+		
+		
+	    // api.getUser(data.UserId, function(err,result){
+	    // 	if(result.errcode!=0){
+	    // 		result = {};
+	    // 		$.extend(result,{avatar:"http://shp.qpic.cn/bizmp/ic7zogWndTyXMsdxYl6QLXpamFlm9FbvzPSokoV6F5BWibqBtK8lTU6Q/"});
+	    // 		console.log(result);
+	    // 		res.render('page/signup',result);
+	    // 		return;
+	    // 	}
+
+	    // 	if(result.errcode ==0){
+	    // 		var keyid = util.format(KEY.USER, result.userid);
+	    // 		// $.extend(result, 
+	    // 		// 	{	
+	    // 		// 		redis.keys
+	    // 		// 	});
+
+	    // 		// redis.set(keyid, result, (err, data) => {
+			  //   // });
+
+	    // 		res.render('page/signup',result);
+	    // 	}
+	    // });
     });
 };
+
+exports.getfetchallusers = function(req, res) {
+ 	api.getDepartmentUsersDetail(1, 1, 0, (err, data)=>{
+	   async.each(data.userlist,(user, rcallback) => {
+             var keyid = util.format(KEY.USER, user.userid);    
+             redis.hmset(keyid, user, (err, data) => {
+             	console.log(data);
+			 });      
+	    },(err) => {
+	       console.log(err);
+	    });
+    });
+    res.send("data.userlist");
+};
+
+
+
+
+
+
+
+
