1b4f072f398adf3da33dc9480b6e6acb6c8e528a
tangwancai <dasikini@163.com>
2016-11-28 15:42:25 +0800

    聊天墙

--- controller/chatwall/wechat.js
+++ controller/chatwall/wechat.js
@@ -1,10 +1,9 @@
 var wechat = require('wechat-enterprise');
 var api = new wechat.API($.config.enterprise.corpId, $.config.enterprise.corpsecret,'41');
 
-exports.gethead = function(req, res) {
+exports.getchat = function(req, res) {
 	var code = req.query.code;
 	var state = req.query.state;
 
-	res.render('page/headscrean');
-	// res.render('page/bubble');
+	res.render('page/wechatwall');
 };
